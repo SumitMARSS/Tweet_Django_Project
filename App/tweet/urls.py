7694228c20f49d3e0183725eba43cb681fc32bb8
sumit <sumit821044@gmail.com>
2025-01-25 15:38:58 +0530

    Create initial interface and functionalities.

---
+++ App/tweet/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path("", views.index, name="index"),
+    path("", views.tweet_list, name="tweet_list"),
+    path("createTweet/", views.create_tweet, name="tweet_create"),
+    path("<int:tweet_id>/tweet_edit/", views.edit_tweet, name="tweet_edit"),
+    path("<int:tweet_id>/tweet_delete/", views.delete_tweet, name="tweet_delete"),
+    path("register/", views.register, name="register"),
+] 
